5848af796c9179a68472f4c1790c2b559fa97c5b
August Lilleaas <august@augustl.com>
2013-01-14 14:24:43 +0100

    Paredit.

--- setup/lisps.el
+++ setup/lisps.el
@@ -1,4 +1,1 @@
-;; (add-hook 'clojure-mode-hook
-;;           (lambda ()
-;;             (paredit-mode t)
-;;             (local-set-key (kbd "S-RET") 'paredit-close-round-and-newline)))
+(add-hook 'clojure-mode-hook (lambda () (paredit-mode 1)))
