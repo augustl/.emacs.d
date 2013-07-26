b0177bcfe894034c3c14fd055dc9d1cbe7da02fb
August Lilleaas <august@augustl.com>
2013-07-26 11:47:04 +0200

    Adding some indent hooks for core.logic.

--- setup/lisps.el
+++ setup/lisps.el
@@ -1,3 +1,8 @@
-(add-hook 'clojure-mode-hook (lambda () (paredit-mode 1)))
+(add-hook 'clojure-mode-hook
+          (lambda ()
+            (paredit-mode 1)
+            (put-clojure-indent 'run* 'defun)
+            (put-clojure-indent 'run 'defun)
+            (put-clojure-indent 'fresh 'defun)
+            (put-clojure-indent 'conde 'defun)))
 (add-to-list 'auto-mode-alist '("\\.edn\\'" . clojure-mode))
-
