b556c5947ee6d69b90ddc06dd558fa8f9a626981
August Lilleaas <august@augustl.com>
2013-07-26 11:35:29 +0200

    Even larger presentation font.

--- themes/presentation.el
+++ themes/presentation.el
@@ -3,4 +3,4 @@
   (set-face-background 'region "#999999")
   (set-background-color "#ffffff")
   (set-foreground-color "#000000")
-  (set-face-attribute 'default nil :height 150))
+  (set-face-attribute 'default nil :height 180))
