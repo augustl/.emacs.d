5a71728e5eec16e64efd72e0b30476d5b1782278
August Lilleaas <august@augustl.com>
2015-04-24 15:16:40 +0200

    Adding more paredit mode hooks.

--- setup/lisps.el
+++ setup/lisps.el
@@ -6,9 +6,13 @@
 
 (add-hook 'clojure-mode-hook
           (lambda ()
-            (paredit-mode 1)
             (put-clojure-indent 'run* 'defun)
             (put-clojure-indent 'run 'defun)
             (put-clojure-indent 'fresh 'defun)
             (put-clojure-indent 'conde 'defun)))
 (add-to-list 'auto-mode-alist '("\\.edn\\'" . clojure-mode))
+
+(add-hook 'lisp-mode-hook #'paredit-mode)
+(add-hook 'emacs-lisp-mode-hook #'paredit-mode)
+(add-hook 'clojure-mode-hook #'paredit-mode)
+(add-hook 'cider-repl-mode-hook #'paredit-mode)
