848a9d459e8c68f5acea5c67274cd8009def31cb
August Lilleaas <august@augustl.com>
2018-09-24 12:12:33 +0200

    Update projectile configs

--- setup/projectile.el
+++ setup/projectile.el
@@ -1,5 +1,5 @@
 (augustl-require-package 'projectile)
 
-(projectile-global-mode)
-(setq projectile-enable-caching t)
-(setq projectile-remember-window-configs t)
+(projectile-mode +1)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+
