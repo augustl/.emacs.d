52acf9083d21c52d6a8aaa77a640d28eb6a2fbf1
August Lilleaas <august@augustl.com>
2026-02-11 22:01:48 +0100

    Fix crash on startup - need to run diff mode setups after diff mode has loaded

--- setup/diff.el
+++ setup/diff.el
@@ -1,3 +1,4 @@
-(set-face-foreground 'diff-added "chartreuse4")
-(set-face-foreground 'diff-hunk-header "RoyalBlue1")
-(set-face-foreground 'diff-removed "red")
+(with-eval-after-load 'diff-mode
+  (set-face-foreground 'diff-added "chartreuse4")
+  (set-face-foreground 'diff-hunk-header "RoyalBlue1")
+  (set-face-foreground 'diff-removed "red"))
